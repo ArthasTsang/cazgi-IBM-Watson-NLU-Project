0c652d7868b385be38c128d019aa4810081454b8
Arthas Tsang <twyarthas@yahoo.com.hk>
2022-05-09 18:32:59 +0800

    update client side

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -2,22 +2,51 @@ import React from 'react';
 import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
-    render() {
-      //Returns the emotions as an HTML table
-      return (  
-        <div>
-          <table className="table table-bordered">
-            <tbody>
-            {
-              /*Write code to use the .map method that you worked on in the 
-              Hands-on React lab to extract the emotions. If you are stuck,
-              please click the instructions to see how to implement a map*/
-            }
-            </tbody>
-          </table>
-          </div>
-          );
-        }
+    
+    state = { emotionList:[] }
+    
+    constructor(props){
+        console.log(props);
+        super(props);
+    }
+    
+    parseEmotion(){
+        /*Write code to use the .map method that you worked on in the 
+                    Hands-on React lab to extract the emotions. If you are stuck,
+                    please click the instructions to see how to implement a map*/
+        let emotionsArr= Object.entries(this.props.emotions);
+        console.log(emotionsArr);
+        return "<tr><td>Under construction</td></tr>";
+    }
+    
+    componentDidMount() {
+        let emotionsArr= Object.entries(this.props.emotions);
+        console.log(emotionsArr);
+        let emotionFormat= emotionsArr.map((emotion)=>{
+            return (
+                    <tr>
+                        <td>{emotion[0]}</td>
+                        <td>{emotion[1]}</td>
+                    </tr>
+            );
+        });
+        this.setState({emotionList:emotionFormat})
+    }
+    
+    render() {        
+        console.log("render emotion table");
+        let emotion= this.parseEmotion();
+        //Returns the emotions as an HTML table
+        return (  
+            <div>
+                <table className="table table-bordered">
+                    <tbody>
+                        {this.state.emotionList}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
     
 }
 export default EmotionTable;
